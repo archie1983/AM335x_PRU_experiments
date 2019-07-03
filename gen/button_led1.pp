7c167e23c0ce7050e0457ddcbf4b6741e56b1a1e
Arturs Elksnis <arturs.elksnis@ggtg.net>
2019-07-03 18:57:32 +0100

    Cleaning up unnecessary files from the repo

--- gen/button_led1.pp
+++
@@ -1,8 +0,0 @@
-gen/decay95.obj: button_led1.c
-gen/decay95.obj: /home/arturs/AE_Custom_Linux_kernels_Yocto/non-yocto_build_envs/ti-cgt-pru_2.1.4/include/stdint.h
-gen/decay95.obj: /home/arturs/AE_Custom_Linux_kernels_Yocto/non-yocto_build_envs/PRU_experiments_AM335x/pru-software-support-package/include/am335x/pru_cfg.h
-gen/decay95.obj: /home/arturs/AE_Custom_Linux_kernels_Yocto/non-yocto_build_envs/PRU_experiments_AM335x/pru-software-support-package/include/am335x/pru_intc.h
-gen/decay95.obj: resource_table_1.h
-gen/decay95.obj: /home/arturs/AE_Custom_Linux_kernels_Yocto/non-yocto_build_envs/ti-cgt-pru_2.1.4/include/stddef.h
-gen/decay95.obj: /home/arturs/AE_Custom_Linux_kernels_Yocto/non-yocto_build_envs/PRU_experiments_AM335x/pru-software-support-package/include/rsc_types.h
-gen/decay95.obj: /home/arturs/AE_Custom_Linux_kernels_Yocto/non-yocto_build_envs/PRU_experiments_AM335x/pru-software-support-package/include/pru_types.h
