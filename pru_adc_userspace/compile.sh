5ae134b644d2cf5bd7c8ead223e5fdc9cc74de8a
Arturs Elksnis <arturs@elksnis.co.uk>
2020-08-11 10:25:33 +0100

    Adding a userspace application to read the output from the PRU firmware and convert it to a human readable form (numbers to ASCII characters).

---
+++ pru_adc_userspace/compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# In addition to the environment variables being included on the next line, I also needed to change Makefile circa line 43:
+# changing line:         $(ARM_CCT)/arm-phytec-linux-gnueabi-gcc -o $(GEN_DIR)/$(PROJ_NAME) $< $(LIBS)
+# into:                  $(CC) -o $(GEN_DIR)/$(PROJ_NAME) $< $(LIBS)
+
+. /home/arturs/CoreLab_Work/Yocto/SDKs/BSP-Yocto-AM335x-PD19.1.1/environment-setup-cortexa8hf-neon-phytec-linux-gnueabi
+
+make all
